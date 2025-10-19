e3f38217e0e370687b165457c9006b6fabac077a
Mariam Mamdouh <mamdouhmariam631@gmail.com>
2025-10-19 23:27:57 +0300

    GetReelByBrandWithSorting

---
+++ Infrastructure.Infrastructure/Services/ReelService.cs
@@ -0,0 +1,87 @@
+﻿using ReelsCommerceSystem.Application.DTOs.Response.Product;
+using ReelsCommerceSystem.Application.DTOs.Response.Reel;
+using ReelsCommerceSystem.Application.Interfaces.Services;
+using ReelsCommerceSystem.Domain.Entities.BrandEntities;
+using ReelsCommerceSystem.Domain.Entities.ReelEntities;
+using ReelsCommerceSystem.Infrastructure.Specifications.Specifications.ReelSpec;
+using ReelsCommerceSystem.Infrastructure.UnitOfWorks;
+using ReelsCommerceSystem.Shared.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReelsCommerceSystem.Infrastructure.Services
+{
+    public class ReelService(IUnitOfWork _unitOfWork) : IReelService
+
+    {
+        public async Task<ApiResponse<List<AllReelsInBrandRes>>> GetReelsByBrandAsync(int brandId, string? sortBy)
+
+        {
+            var brandExists = await _unitOfWork.Repository<Brand>().GetByIdAsync(brandId);
+
+            if (brandExists is null)
+            {
+                return ApiResponse<List<AllReelsInBrandRes>>.ErrorResponse(
+                    HttpStatusCode.NotFound,
+                en: "Brand not found.",
+                ar: "العلامة التجارية غير موجودة.",
+                errors: new List<ValidationError>
+                {
+                    new ValidationError
+                    {
+                        Field = "brandId",
+                        En = "The provided brand ID does not exist.",
+                        Ar = "معرف العلامة التجارية غير موجود."
+                    }
+                }
+            );
+                
+            }
+            var spec = new ReelsByBrandWithSortingSpec(brandId, sortBy);
+            var reels = await _unitOfWork.Repository<Reel>().GetAllWithSpecAsync(spec);
+            var AllReels = new List<AllReelsInBrandRes>();
+            foreach (var reel in reels) 
+            {
+                var result = new AllReelsInBrandRes
+                {
+                    ReelId= reel.Id,
+                    ThumbnailUrl= GenerateThumbnailFromDrive(reel.VideoUrl),
+                    NumOfLikes=reel.NumOfLikes,
+                    NumOfWatches=reel.NumOfWatches,
+                    CreatedAt=reel.CreatedAt,
+                    VideoUrl=reel.VideoUrl,
+                    ProductId=reel.ProductId,
+                    ProductName=reel.Product.Name,
+                    ProductMediaUrl=reel.Product.MediaUrl,
+                    ProductPrice=reel.Product.Price,
+
+                };
+                AllReels.Add(result);
+            
+            }
+            return ApiResponse<List<AllReelsInBrandRes>>
+                .SuccessResponse
+                (
+                                   AllReels,
+                                   HttpStatusCode.OK,
+                                   en: "Request completed successfully.",
+                                   ar: "تم تنفيذ الطلب بنجاح."
+                );
+
+        }
+
+        private string GenerateThumbnailFromDrive(string videoUrl)
+        {
+            var match = Regex.Match(videoUrl ?? "", @"\/d\/(.*?)\/");
+            if (match.Success)
+                return $"https://drive.google.com/thumbnail?id={match.Groups[1].Value}";
+
+            return null; 
+        }
+    }
+}
