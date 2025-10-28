f43f9c026dba24ef7ac382911e955c22caf10956
Esraa-Medhat <esraamedhat1315603@gmail.com>
2025-10-29 02:12:18 +0300

    Add caching to ProductService with IMemoryCache
    Enhanced ProductService by integrating IMemoryCache for product response caching.
    
    Added a private _memoryCache field and updated the constructor to accept an IMemoryCache instance.
    
    Implemented GenerateCacheKey to generate unique keys based on ProductSpecParams.
    
    Updated GetProductsAsync to check for cached results before querying the repository and to store results in cache with a 10-minute sliding expiration.

--- Infrastructure.Infrastructure/Services/ProductService.cs
+++ Infrastructure.Infrastructure/Services/ProductService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Azure;
+using Microsoft.Extensions.Caching.Memory;
 using ReelsCommerceSystem.Application.DTOs.Response.Product;
 using ReelsCommerceSystem.Application.Interfaces.Repositories;
 using ReelsCommerceSystem.Application.Interfaces.Services;
@@ -19,13 +21,20 @@ namespace ReelsCommerceSystem.Infrastructure.Services
     public class ProductService : IProductService
     {
         private readonly IGenericRepository<Product> _genericRepository;
+        private readonly IMemoryCache _memoryCache;
 
-        public ProductService(IGenericRepository<Product> genericRepository)
+        public ProductService(IGenericRepository<Product> genericRepository,IMemoryCache memoryCache)
         {
             _genericRepository = genericRepository;
+            _memoryCache = memoryCache;
         }
         public async Task<ApiResponse<PaginationResponse<ProductPag>>> GetProductsAsync(ProductSpecParams productSpecParams)
         {
+            var cacheKey = GenerateCacheKey(productSpecParams);
+            if(_memoryCache.TryGetValue(cacheKey, out ApiResponse<PaginationResponse<ProductPag>> cachedResponse))
+            {
+                return cachedResponse;
+            }
             var spec = new ProductSpec(productSpecParams);
   
             var products = await _genericRepository.GetAllWithSpecAsync(spec);
@@ -55,12 +64,31 @@ namespace ReelsCommerceSystem.Infrastructure.Services
                 HasNextPage = (productSpecParams.PageIndex ?? 1) * (productSpecParams.PageSize ?? totalCount) < totalCount
             };
 
-            return PaginationResponse<ProductPag>.SuccessResponse(
+            var response= PaginationResponse<ProductPag>.SuccessResponse(
                 data: productDtos,
                 meta: meta,
                 statusCode: HttpStatusCode.OK
             );
+            var cacheOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromMinutes(10));
+            _memoryCache.Set(cacheKey, response, cacheOptions);
+            return response;
 
+        }
+        private static string GenerateCacheKey(ProductSpecParams p)
+        {
+          
+            string key = $"products:" +
+                $"search={p.Search ?? ""};" +
+                $"color={p.Color ?? ""};" +
+                $"size={p.Size ?? ""};" +
+                $"status={p.Status ?? ""};" +
+                $"min={p.MinPrice?.ToString() ?? ""};" +
+                $"max={p.MaxPrice?.ToString() ?? ""};" +
+                $"haveOffer={p.HaveOffer?.ToString() ?? ""};" +
+                $"sort={p.Sort ?? ""};" +
+                $"pageIndex={p.PageIndex ?? 1};" +
+                $"pageSize={p.PageSize ?? 10};";
+            return key;
         }
     }
 }
