282b7f80b37c0bb832e58a2d8ce40164e8b7615f
AbdAlRahman-Hesham <heshamabdalrahman46@gmail.com>
2025-11-28 18:39:35 +0200

    Commit Title: Implement Cart Product Size and Color Options
    Commit Description:
    Adding:
    - Defines DTO for adding items to the cart, including size and color (ReelsCommerceSystem.Application/DTOs/Request/Cart/CartItemReq.cs)
    - Defines DTO for updating cart items, including size and color (ReelsCommerceSystem.Application/DTOs/Request/Cart/UpdateCartItemReq.cs)
    
    Updating:
    - Modified cart logic to incorporate product size and color for add, update, and retrieval operations; integrated product specifications for efficient product data fetching (Infrastructure.Infrastructure/Services/CartService.cs)
    - Removed redundant `using` directives (ReelsCommerceSystem.Api/Controllers/CartController.cs)
    - Updated to reference the new external `CartItemReq` DTO (ReelsCommerceSystem.Application/DTOs/Request/Cart/AddToCartReq.cs)
    - Updated to reference the new external `UpdateCartItemReq` DTO (ReelsCommerceSystem.Application/DTOs/Request/Cart/UpdateCartReq.cs)
    - Updated to include size and color properties and moved to a top-level class; removed `Id` property (ReelsCommerceSystem.Application/DTOs/Response/Cart/CartItemRes.cs)
    - Moved to a top-level class (ReelsCommerceSystem.Application/DTOs/Response/Cart/CartRes.cs)

---
+++ ReelsCommerceSystem.Application/DTOs/Request/Cart/CartItemReq.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReelsCommerceSystem.Application.DTOs.Request.Cart;
+
+public class CartItemReq
+{
+    [Required]
+    public int ProductId { get; set; }
+    
+    [Required]
+    public int Quantity { get; set; }
+    
+    [Required]
+    public string Color { get; set; }
+    
+    [Required]
+    [RegularExpression(@"^(xs|s|m|l|xl|xxl|XS|S|M|L|XL|XXL)$",
+        ErrorMessage = "Size must be: xs|s|m|l|xl|xxl|XS|S|M|L|XL|XXL")]
+    public string Size { get; set; }
+}
