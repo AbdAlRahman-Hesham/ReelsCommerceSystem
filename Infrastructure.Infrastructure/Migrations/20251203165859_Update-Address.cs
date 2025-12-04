3d1b726502b9041b019a34bd9baa2658ff201c96
AbdAlRahman-Hesham <heshamabdalrahman46@gmail.com>
2025-12-04 17:37:26 +0200

    Commit Title: Update Address Entity and Database Configuration
    Commit Description:
    Adding:
    - New migration files for Address entity updates (Infrastructure.Infrastructure/Migrations/20251203165859_Update-Address.Designer.cs)
    - New migration files for Address entity updates (Infrastructure.Infrastructure/Migrations/20251203165859_Update-Address.cs)
    
    Updating:
    - Modified Address entity properties (FirstName to Name, LastName to Postcode) (ReelsCommerceSystem.Domain/Entities/UserEntities/Address.cs)
    - Reflected Address entity property changes (Infrastructure.Infrastructure/Migrations/AppDbContextModelSnapshot.cs)
    
    Configuration:
    - Adjusted connection string retrieval logic (ReelsCommerceSystem.Api/DependencyInjectionExtensions/AddApplicationDatabaseConfig.cs)

---
+++ Infrastructure.Infrastructure/Migrations/20251203165859_Update-Address.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReelsCommerceSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Addresses",
+                newName: "Postcode");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Addresses",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Postcode",
+                table: "Addresses",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Addresses",
+                newName: "FirstName");
+        }
+    }
+}
