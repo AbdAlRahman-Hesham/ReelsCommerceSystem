184f8e61377eb5de6a80a034bdee96a2dad48ac4
NadaReda9 <nreda2799@gmail.com>
2025-12-06 18:23:40 +0200

    searchReelsProducts

---
+++ ReelsCommerceSystem.Api/Controllers/SearchController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReelsCommerceSystem.Application.Interfaces.Services;
+
+namespace ReelsCommerceSystem.Api.Controllers
+{
+    public class SearchController(ISearchService _searchService):AppBaseController
+    {
+        [HttpGet("Search")]
+        public async Task<IActionResult> Search([FromQuery] string text, int pageNumber = 1, int pageSize = 10)
+        {
+            var result = await _searchService.SearchAsync(text, pageNumber, pageSize);
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
