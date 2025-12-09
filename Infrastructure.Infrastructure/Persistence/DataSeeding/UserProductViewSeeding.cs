21f6f58706345e9325c4d00e9e5deb5fbeb1f47f
AbdAlRahman-Hesham <heshamabdalrahman46@gmail.com>
2025-12-09 21:25:40 +0200

    UserProductViewSeeding

---
+++ Infrastructure.Infrastructure/Persistence/DataSeeding/UserProductViewSeeding.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReelsCommerceSystem.Domain.Entities.ProductEntites;
+
+namespace ReelsCommerceSystem.Infrastructure.Persistence.DataSeeding;
+
+public class UserProductViewSeeding : IEntityTypeConfiguration<UserProductView>
+{
+    private static readonly DateTime _staticSeedDate = new DateTime(2025, 12, 9, 0, 0, 0, DateTimeKind.Utc);
+
+    public void Configure(EntityTypeBuilder<UserProductView> builder)
+    {
+        builder.HasData(
+
+          new UserProductView { Id = 1, UserId = "user1", ProductId = 1, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+          new UserProductView { Id = 2, UserId = "user1", ProductId = 2, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+          new UserProductView { Id = 3, UserId = "user1", ProductId = 3, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+
+
+          new UserProductView { Id = 4, UserId = "user2", ProductId = 2, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+          new UserProductView { Id = 5, UserId = "user2", ProductId = 4, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+          new UserProductView { Id = 6, UserId = "user2", ProductId = 5, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+
+
+          new UserProductView { Id = 7, UserId = "user3", ProductId = 1, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+          new UserProductView { Id = 8, UserId = "user3", ProductId = 6, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+          new UserProductView { Id = 9, UserId = "user3", ProductId = 3, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+
+
+          new UserProductView { Id = 10, UserId = "user4", ProductId = 2, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate },
+          new UserProductView { Id = 11, UserId = "user4", ProductId = 5, CreatedAt = _staticSeedDate, UpdatedAt = _staticSeedDate }
+      );
+
+    }
+}
