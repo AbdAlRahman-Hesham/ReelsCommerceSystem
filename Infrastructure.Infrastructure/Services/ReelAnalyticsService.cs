85e8245be30a8c5611855a0eb4ef5456ee5eaeaa
Esraa-Medhat <esraamedhat1315603@gmail.com>
2025-12-11 00:38:43 +0200

    Top Brands by Reels Views
    Extended GenericRepository with IQueryable support via GetAllQueryable.
    Added ReelAnalyticsService and IReelAnalyticsService to compute
    top brands based on reel views in the last week. Introduced
    TopBrandDto for structured response. Updated ReelController
    with a new GetTopBrands endpoint. Enhanced IGenericRepository
    to include GetAllQueryable method.

---
+++ Infrastructure.Infrastructure/Services/ReelAnalyticsService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReelsCommerceSystem.Application.DTOs.Response.Brand;
+using ReelsCommerceSystem.Application.Interfaces.Repositories;
+using ReelsCommerceSystem.Application.Interfaces.Services;
+using ReelsCommerceSystem.Domain.Entities.ReelEntities;
+
+namespace ReelsCommerceSystem.Infrastructure.Services
+{
+    public class ReelAnalyticsService : IReelAnalyticsService
+    {
+        private readonly IGenericRepository<UserReelView> _userReelViewRepo;
+
+        public ReelAnalyticsService(IGenericRepository<UserReelView> userReelViewRepo)
+        {
+            _userReelViewRepo = userReelViewRepo;
+        }
+        public async Task<List<TopBrandDto>> GetTopBrandsLastWeekAsync(int topN)
+        {
+            var lastWeek = DateTime.UtcNow.AddDays(-7);
+
+           
+            var recentViewsQuery = _userReelViewRepo.GetAllQueryable()
+                .Where(v => v.CreatedAt >= lastWeek)
+                .Include(v => v.Reel)
+                .ThenInclude(r => r.Brand);
+
+            var topBrands = await recentViewsQuery
+                .GroupBy(v => new { v.Reel.BrandId, v.Reel.Brand.DisplayName })
+                .Select(g => new TopBrandDto
+                {
+                    BrandId = g.Key.BrandId,
+                    BrandName = g.Key.DisplayName,
+                    TotalViews = g.Count()
+                })
+                .OrderByDescending(b => b.TotalViews)
+                .Take(topN)
+                .ToListAsync();
+
+            return topBrands;
+
+        }
+    }
+}
--- ReelsCommerceSystem.Api/Controllers/ReelController.cs
+++ ReelsCommerceSystem.Api/Controllers/ReelController.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ReelsCommerceSystem.Application.Interfaces.Services;
+using ReelsCommerceSystem.Infrastructure.Services;
 
 namespace ReelsCommerceSystem.Api.Controllers;
 
-public class ReelController(IReelService _reelService) :AppBaseController
+public class ReelController(IReelService _reelService,IReelAnalyticsService _reelAnalyticsService) :AppBaseController
 {
     [HttpGet("{brandId}")]
     public async Task<IActionResult> GetReelsByBrand(int brandId, [FromQuery] string? sortBy = null)
@@ -11,4 +12,25 @@ public class ReelController(IReelService _reelService) :AppBaseController
         var response = await _reelService.GetReelsByBrandAsync(brandId, sortBy);
         return StatusCode(response.StatusCode, response);
     }
-}
+
+
+
+    [HttpGet("topbrands")]
+    public async Task<IActionResult> GetTopBrands([FromQuery] int topN = 5)
+    {
+        if (topN <= 0)
+            return BadRequest(new { message = "The number of top brands must be greater than zero." });
+
+        var topBrands = await _reelAnalyticsService.GetTopBrandsLastWeekAsync(topN);
+
+        return Ok(new
+        {
+            topN,
+            brands = topBrands
+        });
+    }
+
+
+  
+    }
+
